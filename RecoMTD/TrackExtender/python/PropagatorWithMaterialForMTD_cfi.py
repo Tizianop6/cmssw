94ebaefc73df87eb4ec4e7ae990aabf04f35d112
Tizianop6 <tizianop6@gmail.com>
2025-03-21 12:51:41 +0100

    increase maxDPhi in propagator

--- RecoMTD/TrackExtender/python/PropagatorWithMaterialForMTD_cfi.py
+++ RecoMTD/TrackExtender/python/PropagatorWithMaterialForMTD_cfi.py
@@ -2,7 +2,7 @@ import FWCore.ParameterSet.Config as cms
 
 PropagatorWithMaterialForMTD = cms.ESProducer(
     "PropagatorWithMaterialESProducer",
-    MaxDPhi = cms.double(1.6),     #default was 1.6
+    MaxDPhi = cms.double(3.14),     #default was 1.6
     ComponentName = cms.string('PropagatorWithMaterialForMTD'),
     Mass = cms.double(0.13957018),     #default was 0.105
     PropagationDirection = cms.string('anyDirection'),
