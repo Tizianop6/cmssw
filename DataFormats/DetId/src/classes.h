8db58676652b680ede8a7c695c795ca118b30d8f
Tiziano <tizianop6@gmail.com>
2026-05-25 16:33:26 +0200

    added MergedClusters to standard reconstruction/simulation

--- DataFormats/DetId/src/classes.h
+++ DataFormats/DetId/src/classes.h
@@ -6,4 +6,3 @@
 #include "DataFormats/DetId/interface/DetIdVector.h"
 #include "DataFormats/DetId/interface/DetIdCollection.h"
 #include "DataFormats/Common/interface/DetSetVector.h"
-#include "DataFormats/Common/interface/DetSetVectorNew.h"
